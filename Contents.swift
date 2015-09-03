07fc7cab357cfd3b65b4d9c8d6c7761d9b6a165a
felixMCC <nsotresmccpersonal@gmail.com>
2015-09-03 17:18:13 -0500

    CriticalThinkingWeek3FelixS
    Critical Thinking Week 3: Sailboat simple image.

---
+++ Contents.swift
@@ -0,0 +1,51 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Define constants
+let boat : String = "\u{26F5}"; let sun : String = "\u{26C5}"; let waves : String = "^^"; let cloud : String = "\u{2601}"; let message: String = "*** I LOVE SAILING ***"
+
+
+
+
+
+//define variables for computation
+var animationCounter = 0; var cloudCounter = 0
+
+
+//define variables for display grid (5 x 3)
+
+var R1C1 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R1C2 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R1C3 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R1C4 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R1C5 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"
+
+
+
+
+var R2C1 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R2C2 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R2C3 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R2C4 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R2C5 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"
+
+
+
+
+
+var R3C1 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R3C2 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R3C3 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R3C4 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R3C5 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"
+
+
+
+
+
+var R4C1 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R4C2 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R4C3 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R4C4 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"; var R4C5 = "\u{000B}" + "\u{0020}" + "\u{0020}" + "\u{0020}" + "\u{0020}"
+
+
+
+
+
+R1C1 = sun; R3C3 = boat;
+
+R4C1 = waves; R4C2 = waves; R4C3 = waves; R4C4 = waves; R4C5 = waves;
+
+println(message)
+println(R1C1 + R1C2 + R1C3 + R1C4 + R1C5)
+println(R2C1 + R2C2 + R2C3 + R2C4 + R2C5)
+println(R3C1 + R2C2 + R3C3 + R3C4 + R3C5)
+println(R4C1 + R4C2 + R4C3 + R4C4 + R4C5)
